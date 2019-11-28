918fdc55b82109d73ebf0d7954687832f5d72774
svjense <sven@s7j.de>
2019-11-28 01:15:00 +0000

    Test

--- node/server.js
+++ node/server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World and Moon!\n");
+  res.end("Hello World!\n");
 
 }).listen(port);
 
